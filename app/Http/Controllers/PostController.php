c9a4a1182c23154d2b91c55092a5310397118d9c
subdanial <subdanial@gmail.com>
2020-10-13 12:38:34 +0330

    destroy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -123,6 +123,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return  redirect()->back();
     }
 }
